4ba601d3533aa4b39a70eda1604c4a5dc8ea52a3
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2026-03-05 00:08:19 -0500

    Make absence way more obvious

--- src/components/home-assistant/presence.tsx
+++ src/components/home-assistant/presence.tsx
@@ -4,9 +4,9 @@ import { HomeAssistantContext } from "./home-assistant-context";
 export default function Presence() {
   const hassContext = useContext(HomeAssistantContext);
 
-  return (
-    <div
-      className={`absolute bottom-0 left-0 m-3 w-5 h-5 rounded-full  ${hassContext?.present ? "bg-green-500" : "bg-red-500"}`}
-    />
+  return hassContext?.present ? (
+    <div className="absolute bottom-0 left-0 m-3 w-5 h-5 rounded-full bg-green-500" />
+  ) : (
+    <div className="absolute top-0 w-full h-full -z-100 bg-red-500" />
   );
 }
