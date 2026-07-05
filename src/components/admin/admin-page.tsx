ed4289330ae84cfb3c1670188ca6622fd06d52b9
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2026-07-05 14:24:45 -0400

    Update admin page to be nicer

--- src/components/admin/admin-page.tsx
+++ src/components/admin/admin-page.tsx
@@ -5,15 +5,25 @@ function AdminPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="m-5">
-      <h1 className="text-xl text-bold pb-5">AdminPage</h1>
+    <div className="p-5 h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-6xl text-bold pb-5 ">Debug Tools</h1>
       <Button
+        className="w-48 h-18  text-3xl"
         onClick={() => {
           navigate("/");
           window.location.reload();
         }}
       >
         Refresh
+      </Button>
+      <Button
+        variant="secondary"
+        className="w-48 h-18 text-3xl"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Back
       </Button>
     </div>
   );
