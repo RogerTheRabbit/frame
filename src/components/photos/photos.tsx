c5e51920036361739836dab46b76ff6a25d15d78
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2026-08-02 17:59:25 -0400

    Add fade for smoother image switching

--- src/components/photos/photos.tsx
+++ src/components/photos/photos.tsx
@@ -1,13 +1,10 @@
-import {
-  AssetTypeEnum,
-  init,
-  searchRandom,
-  type AssetResponseDto,
-} from "@immich/sdk";
+import { AssetTypeEnum, init, searchRandom } from "@immich/sdk";
 import { useEffect, useState } from "react";
 
 const API_KEY = import.meta.env.VITE_IMMICH_API_KEY;
 const BASE_URL = `${import.meta.env.PROD ? import.meta.env.BASE_URL : ""}/api`;
+const REPEAT_COUNT = 10;
+const IMAGE_DURATION_SECONDS = 60;
 
 init({
   baseUrl: BASE_URL,
@@ -15,9 +12,7 @@ init({
 });
 
 function Photos() {
-  const [assets, setAssets] = useState<AssetResponseDto[]>([]);
-  const [idx, setIdx] = useState(0);
-  const secondaryImageIdx = (idx + 1) % assets.length;
+  const [queue, setQueue] = useState<string[]>([]);
 
   const searchAssets = () => {
     searchRandom({
@@ -29,10 +24,14 @@ function Photos() {
       },
     })
       .then((resp) => {
-        setAssets(
-          resp.filter((asset) => asset.people && asset.people.length > 1),
+        const filteredResp = resp.filter(
+          (asset) => asset.people && asset.people.length > 1,
         );
-        setIdx(0);
+        let toAppend: string[] = Array(REPEAT_COUNT)
+          .fill(filteredResp)
+          .flat()
+          .map((asset) => asset.id);
+        setQueue([...queue, ...toAppend]);
       })
       .catch((err) => {
         console.error("Failed to smart search", err);
@@ -41,35 +40,37 @@ function Photos() {
 
   useEffect(() => {
     searchAssets();
-    const interval = setInterval(() => {
-      searchAssets();
-    }, 3600000);
-    return () => {
-      clearInterval(interval);
-    };
   }, []);
 
   useEffect(() => {
+    if (queue.length < 5) {
+      searchAssets();
+    }
+
     const timeout = setTimeout(() => {
-      setIdx((i) => (i + 1) % assets.length);
-    }, 60000);
+      setQueue((q) => q.slice(1));
+    }, IMAGE_DURATION_SECONDS * 1000);
 
     return () => {
       clearTimeout(timeout);
     };
-  }, [assets, idx]);
+  }, [queue]);
 
   return (
     <div className="w-full h-full absolute top-0 left-0 overflow-hidden -z-100">
       <img
-        key={assets[idx]?.id}
-        className="w-full brightness-50 inset-0 h-[150vh] w-full object-cover animate-[scroll-y_60s_ease-in-out] [animation-fill-mode:forwards]"
-        src={`${import.meta.env.VITE_IMMICH_SERVER_URL}/api/assets/${assets[idx]?.id}/thumbnail?size=fullsize&apiKey=${import.meta.env.VITE_IMMICH_API_KEY}`}
+        key={queue[0]}
+        className={
+          "w-full brightness-50 inset-0 h-[150vh] w-full object-cover animate-[scroll-y_60s_ease-in-out] [animation-fill-mode:forwards] transition-opacity duration-500 opacity-100"
+        }
+        src={`${import.meta.env.VITE_IMMICH_SERVER_URL}/api/assets/${queue[0]}/thumbnail?size=fullsize&apiKey=${import.meta.env.VITE_IMMICH_API_KEY}`}
       />
-      {/* Predownload image so it renders faster on next  */}
       <img
-        key={assets[secondaryImageIdx]?.id}
-        src={`${import.meta.env.VITE_IMMICH_SERVER_URL}/api/assets/${assets[secondaryImageIdx]?.id}/thumbnail?size=fullsize&apiKey=${import.meta.env.VITE_IMMICH_API_KEY}`}
+        key={queue[1]}
+        className={
+          "w-full brightness-50 inset-0 h-[150vh] w-full object-cover transition-opacity duration-500 opacity-0"
+        }
+        src={`${import.meta.env.VITE_IMMICH_SERVER_URL}/api/assets/${queue[1]}/thumbnail?size=fullsize&apiKey=${import.meta.env.VITE_IMMICH_API_KEY}`}
       />
     </div>
   );
